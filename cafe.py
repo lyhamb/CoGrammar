85e82d9271552e074a83de0c45c0092b5463ff7b
witchalls <46869460+witchalls@users.noreply.github.com>
2024-01-19 17:28:32 +0000

    Finished cafe task

--- cafe.py
+++ cafe.py
@@ -2,12 +2,27 @@
 
 # Create menu list with four cafe items.
 menu = ["Espresso", "Americano", "Tea", "Orange Juice"]
+
 # Create dictionary with stock values per item.
 stock = {
-    'Espresso' : 40,
-    'Americano': 50,   
+    "Espresso" : 40,
+    "Americano" : 50,
+    "Tea" : 100,
+    "Orange Juice" : 20
     }
 
-print(stock['Espresso'])
+# Create dictionary with price (£) per item.
+price = {
+    "Espresso" : 3.50,
+    "Americano" : 4.00,
+    "Tea" : 2.00,
+    "Orange Juice" : 4.80
+    }
 
-# Creates dictionary with prices per item.
+# Loop to calculate total stock value.
+total_stock = 0
+for item in menu:
+    total_stock += price[item] * stock[item]
+
+print(f"The total value of the stock is: £{total_stock}")
+    
