8e307a0eda075dfc2158d12617810442820cbf92
witchalls <46869460+witchalls@users.noreply.github.com>
2024-01-21 16:56:45 +0000

    Added formatting to total_stock

--- cafe.py
+++ cafe.py
@@ -1,4 +1,4 @@
-# === Data Structures - Practical Task === #
+# ======== Data Structures - Practical Task ======== #
 
 # Create menu list with four cafe items.
 menu = ["Espresso", "Americano", "Tea", "Orange Juice"]
@@ -14,15 +14,19 @@ stock = {
 # Create dictionary with price (£) per item.
 price = {
     "Espresso" : 3.50,
-    "Americano" : 4.00,
+    "Americano" : 4.35,
     "Tea" : 2.00,
     "Orange Juice" : 4.80
     }
 
-# Loop to calculate total stock value.
+# For loop to calculate total stock value.
 total_stock = 0
 for item in menu:
     total_stock += price[item] * stock[item]
 
-print(f"The total value of the stock is: £{total_stock}")
-    
+# Format to ensure 2 decimal places for currency (e.g. 230.5 would become 230.50).
+# Rounding to 2 d.p in case any non-integers are used for stock values. 
+total_stock_currency = '{:.2f}'.format(round(total_stock, 2))
+
+# Output result
+print(f"The total value of stock is: £{total_stock_currency}")  
