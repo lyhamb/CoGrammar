935ecd5075c6a9f5dc46109e1b9f70595680d6c8
witchalls <46869460+witchalls@users.noreply.github.com>
2024-01-27 18:38:28 +0000

    Added error handling to DOB task

--- dob_task.py
+++ dob_task.py
@@ -6,21 +6,29 @@ birthdates = []
 
 # Open DOB.txt and convert each line to a list of the elements.
 # Append first two elements to names list and remaining elements to birthdates.
-with open ('./DOB.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        line_list = line.strip().split(' ')
-        names.append(line_list[:2])
-        birthdates.append(line_list[2:])
+try:
+    with open ('./DOB.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            line_list = line.strip().split(' ')
+            names.append(line_list[:2])
+            birthdates.append(line_list[2:])
+except FileNotFoundError:
+    print("File not found. There could be a problem with the file path.")
+except IOError as e:
+    print(f"An error occurred while reading the file: {e}")
+except Exception as e:
+    print(f"The following error occurred: {e}")
+else:
+    print("File read successfully!\n")
 
+    # Output heading using ANSI escape code for bold console text (\033[1m).
+    print("\033[1mName \033[0m")
 
-# Output heading using ANSI escape code for bold console text (\033[1m).
-print("\033[1mName \033[0m")
+    # Output list of names on a new line after joining the list. 
+    for name in names:
+        print(' '.join(name))
 
-# Output list of names on a new line after joining the list. 
-for name in names:
-    print(' '.join(name))
-
-# Repeat the same process birthdates list. \n  included to separate the sections.
-print("\n\033[1mBirthdate \033[0m")
-for date in birthdates:
-    print(' '.join(date))
+    # Repeat the same process birthdates list. \n  included to separate the sections.
+    print("\n\033[1mBirthdate \033[0m")
+    for date in birthdates:
+        print(' '.join(date))
