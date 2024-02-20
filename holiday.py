36aff3e8c80aa590a985621c4848a9cd00969071
witchalls <46869460+witchalls@users.noreply.github.com>
2024-02-20 10:56:24 +0000

    Small changes

--- holiday.py
+++ holiday.py
@@ -3,7 +3,7 @@
 # List of city destinations. Flight prices must be updated in plane_cost function.
 cities = ['Paris', 'Cape Town', 'Palermo', 'Singapore']
 
-# ===== Function declarations =======
+# ========== Functions ==================================
 
 
 def hotel_cost(num_nights):
@@ -32,34 +32,35 @@ def car_rental(rental_days):
     return rental_price * rental_days
 
 
-def holiday_costs(hotel, plane, car, num_nights, rental_days, city_flight):
+def holiday_costs(hotel_cost, plane_cost, car_rental, num_nights, city_flight, rental_days):
     ''' Outputs cost breakdown and returns total holiday cost (float)'''
-    hotel_total = hotel(num_nights)
-    car_total = car(rental_days)
-    plane_total = plane(city_flight)
+    hotel_total = hotel_cost(num_nights)
+    car_total = car_rental(rental_days)
+    plane_total = plane_cost(city_flight)
     print("----------------------------------------------\n"
           f"Cost breakdown for your holiday in {city_flight}:\n"
-          f"Cost of Flight: £{plane_total}\n"
-          f"Total hotel cost ({num_nights} nights): £{hotel_total}\n"
-          f"Car rental ({rental_days} days): £{car_total}\n"
+          f"Cost of Flight: £{plane_total:.2f}\n" +
+          f"Total hotel cost ({num_nights} nights): £{hotel_total:.2f}\n"
+          f"Car rental ({rental_days} days): £{car_total:.2f}\n"
           "----------------------------------------------")
     return hotel_total + car_total + plane_total
 
 
 def get_valid_int(bounds=None):
-    '''Gets user input and returns user_input if it is an integer.
+    '''Gets user input and returns the input if it is an integer.
        Takes optional bounds argument to validate the range of the input.
     '''
     while True:
         try:
             user_input = int(input())
+            # Check if the user has passed in a bounds argument.
             if bounds is not None:
                 lower, upper = bounds
                 if user_input >= lower and user_input <= upper:
                     return user_input
                 else:
-                    print(f"Out of range: Please enter a number from 
-                          {lower}-{upper}")
+                    print("Out of range: Please enter a number from" +
+                          f" {lower}-{upper}")
             else:
                 return user_input
         except ValueError:
@@ -71,18 +72,20 @@ def menu(city_list):
     for index, city in enumerate(city_list, 1):
         print(index, ':', city)
 
-    print("Please choose a destination from the list below.")
+    # Get data from user and call holiday_costs function to output cost breakdown and total cost. 
+    print("Please choose a destination (1-4) from the list below.")
     user_index = get_valid_int(bounds=(1, 4))
     city_flight = city_list[user_index - 1]
     print(f"How many nights would you like to stay in {city_flight}?")
     num_nights = get_valid_int()
     print("How many days would you like to hire a car for? (Enter 0 if you don't need a car)")
     rental_days = get_valid_int()
-    print("The total cost of the holiday is: £",
-          holiday_costs(hotel_cost, plane_cost, car_rental,
-                        num_nights, rental_days, city_flight),
+    print("The total cost of the holiday is: " +
+          f"£{holiday_costs(hotel_cost, plane_cost, car_rental,
+                            num_nights, city_flight, rental_days):.2f}" +
           "\n----------------------------------------------")
 
 
-# Call menu function to start program.
-menu(cities)
+# Call menu function to start program if file is executed.
+if __name__ == "__main__":
+    menu(cities)
